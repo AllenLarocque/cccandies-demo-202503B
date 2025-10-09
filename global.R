c81ea3ecde02497aa6b54c91e999618ff9c36a71
Allen Larocque <allen.larocque@gmail.com>
2025-10-09 12:17:36 -0700

    SCFM reprex

--- global.R
+++ global.R
@@ -1,43 +1,38 @@
 # minimal demo of a working SpaDES model with harvesting (using ws3)
 repos <- c("https://predictiveecology.r-universe.dev", getOption("repos"))
 source("https://raw.githubusercontent.com/PredictiveEcology/pemisc/refs/heads/development/R/getOrUpdatePkg.R")
-getOrUpdatePkg(c("Require", "SpaDES.project"), c("1.0.1.9003", "0.1.1.9037")) # only install/update if required
+getOrUpdatePkg(c("Require", "SpaDES.project","reticulate"), c("1.0.1.9003", "0.1.1.9037","1.43.0")) # only install/update if required
 
 # Require::Install("PredictiveEcology/Require@hasHEAD (>=0.1.1.9019)", install = "force")
 # Require::Require("PredictiveEcology/reproducible@AI (HEAD)")
-
+Require::Require("reticulate")   # Necessary to run the optimizer logic below; probably bad and I need to move this back into the setupProject call
 Require::setLinuxBinaryRepo()
 
 ################################################################################
 
-# Settings for WS3:
-# Select scheduler mode:
-scheduler.mode <- "optimize"
-scheduler.mode<- "areacontrol"
-
-# decide on target.scalefactors depending on scheduler.mode
-if (scheduler.mode == "optimize") {
-  target.scalefactors <- py_dict(basenames, list(rep(1.0, times = length(basenames))))
-} else if (scheduler.mode == "areacontrol") {
-  target.scalefactors <- NULL
-}
-
-################################################################################
-# setwd("~/projects")
 out <- SpaDES.project::setupProject(
 
   ### Define local variables (spades_ws3 module parameters)
-  scheduler.mode = scheduler.mode,             # currently 'areacontrol' and 'optimize' is implemented
-  target.scalefactors = target.scalefactors,   # these are different depending on scheduler.mode
+  basenames = list("tsa41"),
+  #scheduler.mode = "optimize", # Change these to set scheduler mode between 'areacontrol' and 'optimize'
+  scheduler.mode<- "areacontrol",
+  target.scalefactors={                # these are different depending on scheduler.mode selection
+  if (scheduler.mode == "optimize") {
+    py_dict(basenames, list(rep(1.0, times = length(basenames))))
+  } else if (scheduler.mode == "areacontrol") {
+    NULL
+  }
+  },
 
   base.year = 2020,           # first year of harvest planning
-  basenames = list("tsa41","tsa40"),  # the 'basenames' to run the simulation
-  horizon = 3,               # The number of planning periods to include in optimization.
-  period_length = 8,         # The number of years between planning periods. Sept 2025: Greg says 'don't change this unless you know what you are doing'
+  horizon = 10,               # The number of planning periods to include in optimization.
+  period_length = 10,         # The number of years between planning periods. Sept 2025: Greg says 'don't change this unless you know what you are doing'
+  times = list(start = 0, end = 99),                    # used in scfm and LandR. This determines how long the simulation will run
 
   shp.path = "gis/shp",    # path to GIS shape files
   tif.path = "tif",           # path to tifs within the input directory
   target.masks = list(c('? ? ? ?')), # do not modify. AL: I don't know what this is
+
   ###
 
   useGit = "eliotmcintire",
@@ -50,15 +45,15 @@ out <- SpaDES.project::setupProject(
     #"UBC-FRESH/cccandies_demo_input@master", #add this as a submodule to ws3_dataInit
     "PredictiveEcology/spades_ws3_dataInit@dev",
     "PredictiveEcology/spades_ws3@dev",
-    "AllenLarocque/spades_ws3_landrAge@PE"
-    # "PredictiveEcology/scfm@development",
+    "AllenLarocque/spades_ws3_landrAge@PE",
+    "PredictiveEcology/scfm@development"
     # "PredictiveEcology/Biomass_borealDataPrep@development",
     # "PredictiveEcology/Biomass_core@development",
     # "PredictiveEcology/Biomass_regeneration@development",
     # "ianmseddy/LandR_reforestation@master"
   ),
 
-  times = list(start = 0, end = 99),                    # used in scfm and LandR
+
   options = list(spades.allowInitDuringSimInit = TRUE), # set to true to allow for the running of Init events during simInit
   # outputs = data.frame(objectName = "landscape"), # do not modify
   params = list(
@@ -98,6 +93,12 @@ out <- SpaDES.project::setupProject(
   }
 )
 
+ #fix modules
+  out$modules <- c(
+    grep("scfm", out$modules, invert = TRUE, value = TRUE),
+    "scfm/modules/scfmDataPrep", "scfm/modules/scfmDiagnostics",
+    "scfm/modules/scfmIgnition", "scfm/modules/scfmEscape", "scfm/modules/scfmSpread"
+    )
 
 simOut <- do.call(SpaDES.core::simInitAndSpades, out)
 
@@ -108,17 +109,12 @@ simOut <- do.call(SpaDES.core::simInitAndSpades, out)
 # import pdb; pdb.set_trace() #put this chunk in to debug python
 #to update ws3, pip install --upgrade ws3
 
-# TODO:
+#TODO:
 #TODO: make harvestStats a data.table not a data.frame
 
 
 
-# fix modules
-# out$modules <- c(
-#   grep("scfm", out$modules, invert = TRUE, value = TRUE),
-#   "scfmDataPrep", "scfmDiagnostics",
-#   "scfmIgnition", "scfmEscape", "scfmSpread"
-#   )
+
 #out$modules <- out$modules[grep("cccandies_demo_input", out$modules, invert = TRUE)]   # Fix this
 
 
