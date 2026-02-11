9d0cd5d4763ed3bee7e506d4d09028e439602df8
Allen Larocque <allen.larocque@gmail.com>
2026-02-10 20:17:51 -0800

    Delete folder

--- global.R
+++ global.R
@@ -45,7 +45,7 @@ modules = c(
   "AllenLarocque/spades_ws3_dataInit@dev",
   "AllenLarocque/spades_ws3@dev",
   "AllenLarocque/spades_ws3_landrAge@PE",
-  "AllenLarocque/scfm@development",                 # TEMP: this scfm branch contains 'fire multiplier' features
+  "AllenLarocque/scfm@development"                 # TEMP: this scfm branch contains 'fire multiplier' features
   #"AllenLarocque/spades_ws3_diag_FRESH@main",        # This module contains diagnostic plots for the WS3_FRESH module
   #"AllenLarocque/spades_ws3_scfm_diag_FRESH@main"    # This module contains diagnostic plots for the WS3_FRESH_scfm module
   #"PredictiveEcology/Biomass_borealDataPrep@development",
